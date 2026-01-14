5bf32267f50df7df06f9f20e171d65d8bd01515d
MathiasGoris2440 <mathias.goris@outlook.com>
2026-01-14 19:14:30 +0100

    Remove old api

--- HotelBooking.API/Services/Interfaces/IHotelSearchService.cs
+++
@@ -1,9 +0,0 @@
-﻿using HotelBookingAPI.Models.DTOs;
-
-namespace HotelBookingAPI.Services.Interfaces;
-
-public interface IHotelSearchService
-{
-    Task<IEnumerable<HotelSearchResponse>> SearchHotelsAsync(HotelSearchRequest request);
-    Task<HotelSearchResponse?> GetHotelDetailsAsync(int hotelId, DateTime checkInDate, DateTime checkOutDate, int numberOfGuests);
-}
